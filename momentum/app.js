6becb588748ad7346b556abf72030107b3e97cb4
LEE2302 <dltpdudqkqh1@gmail.com>
2023-01-15 00:16:16 +0900

    노마드 코더

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,16 @@
-alert('hi')
+const logInFrom = document.querySelector('#login_form');
+const logInInput = document.querySelector('#login_form input');
+const logInH1 = document.querySelector('h1.hidden');
+
+function onLogInSubmit (event) {
+    //기본동작을 막아주는
+    event.preventDefault();
+    const userName = logInInput.value;
+    logInFrom.classList.add('hidden');
+    logInH1.innerHTML = `hello ${userName}`;
+    logInH1.classList.remove('hidden');
+
+    console.log(userName);
+};
+
+logInFrom.addEventListener('submit', onLogInSubmit);
